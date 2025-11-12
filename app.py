db8a8b5b93fe1426f4fabdce052fdd6f4e21aeb2
Erika Rocha <erikaarosio@hotmail.com>
2025-11-12 11:16:08 -0300

    definir função main

--- app.py
+++ app.py
@@ -1,36 +1,47 @@
-# importar biblioteca
-import os
-
-print('''Sabor Express
-''')
-
-print('1. Cadastrar restaurante')
-print('2. Listar restaurantes')
-print('3. Ativar restaurante')  
-print('4. Sair\n')
-
 # não precisa definir tipo
 # padrao de nomeclatura: snake_case (variáveis, funções e métodos)
 # Aceita aspas duplas ou simples
 
-opcao_escolhida = int(input('Escolha uma opção: '))
+# importar biblioteca
+import os
 
-# por padrão o input recebe uma string
-# print(type(opcao_escolhida))
+# definir funções
+def exibir_nome_do_programa():
+    print('''Sabor Express
+    ''')
 
-# definir função
+def exibir_menu():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurante')  
+    print('4. Sair\n')
 
 def finalizar_app():
     # os.system('clear') no mac
     os.system('cls')    
-    print('Finalizando o aplicativo')    
+    print('Finalizando o aplicativo')  
 
-if opcao_escolhida == 1:
-    print('Cadastrar restaurante')
-elif opcao_escolhida == 2:
-    print('Listar restaurantes')
-elif opcao_escolhida == 3:
-    print('Ativar restaurante')
-else: 
-    finalizar_app()
+# por padrão o input recebe uma string
+# print(type(opcao_escolhida))
 
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurante')
+    else: 
+        finalizar_app()
+
+# definir como programa principal
+
+def main():
+    exibir_nome_do_programa()
+    exibir_menu()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
