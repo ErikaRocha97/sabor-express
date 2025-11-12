f43817e3dcf196d6d2c3fa12fa93b645dce94055
Erika Rocha <erikaarosio@hotmail.com>
2025-11-12 10:29:51 -0300

    Entrada e saída de dados

---
+++ app.py
@@ -0,0 +1,16 @@
+print('''Sabor Express
+''')
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurantes')
+print('3. Ativar restaurante')  
+print('4. Sair\n')
+
+# não precisa definir tipo
+# padrao de nomeclatura: snake_case (variáveis, funções e métodos)
+# Aceita aspas duplas ou simples
+
+opcao_escolhida = input('Escolha uma opção: ')
+
+print(f'Você escolheu a opção {opcao_escolhida}')
+
