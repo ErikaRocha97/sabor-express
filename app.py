36b60fd764326ef42093c5befc82fdd0b7365cd5
Erika Rocha <erikaarosio@hotmail.com>
2025-11-12 11:45:16 -0300

    criar função opção inválida

--- app.py
+++ app.py
@@ -21,27 +21,36 @@ def finalizar_app():
     os.system('cls')    
     print('Aplicativo finalizado')  
 
+def opcao_invalida():
+    print('Opção inválida!\n')
+    input('Pressione enter para voltar ao menu principal.')
+    main()
+
 # por padrão o input recebe uma string
 # print(type(opcao_escolhida))
 
 def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
 
-    match opcao_escolhida:
-        case 1:
-            print('Adicionar restaurante')
-        case 2:
-            print('Listar restaurantes')
-        case 3:
-            print('Ativar restaurante')
-        case 4:
-            finalizar_app()
-        case _:
-            print('Opção inválida!')
+        match opcao_escolhida:
+            case 1:
+                print('Adicionar restaurante')
+            case 2:
+                print('Listar restaurantes')
+            case 3:
+                print('Ativar restaurante')
+            case 4:
+                finalizar_app()
+            case _:
+                opcao_invalida()
+    except:
+        opcao_invalida()
 
 # definir como programa principal
 
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_menu()
     escolher_opcao()
